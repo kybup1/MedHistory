22c424b1731454a9b96709c15e3c63cf096be552
gfels6 <sascha.gfeller@gmail.com>
2019-01-08 15:50:56 +0100

    added first draft of styling

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="home">
-    <div class="header">
-      <h1>Eingeloggt als: </h1>
-      <h2 v-if="practLoaded">{{pract.name[0].given[0] + " " + pract.name[0].family}}</h2>
-      <h2 v-else>Laden...</h2>
-    </div>
-    <button v-on:click="logout">Ausloggen</button>
+  <div>
+  <header>
+    <h3>MedHistory</h3>
+  </header>
+
+  <section>
+  <nav>
     <div class="patList">
       <div v-if="patListLoaded">
         <ul>
@@ -17,6 +17,26 @@
 
       </div>
     </div>
+  </nav>
+  
+  <article>
+    <div class="home">
+      <div class="header">
+        <h1>Eingeloggt als: </h1>
+        <h2 v-if="practLoaded">{{pract.name[0].given[0] + " " + pract.name[0].family}}</h2>
+        <h2 v-else>Laden...</h2>
+      </div>
+      <button v-on:click="logout">Ausloggen</button>
+    </div>
+  </article>
+</section>
+
+<footer>
+  <p>Footer</p>
+</footer>
+
+
+
   </div>
 </template>
 
@@ -215,3 +235,70 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+
+* {
+  box-sizing: border-box;
+}
+
+h3 {
+  margin: 0;
+}
+/* Style the header */
+header {
+  background-color: #666;
+  padding: 30px;
+  text-align: center;
+  font-size: 35px;
+  color: white;
+  height: 100px;
+}
+
+/* Create two columns/boxes that floats next to each other */
+nav {
+  float: left;
+  width: 20%;
+  height: 300px; /* only for demonstration, should be removed */
+  background: #ccc;
+  padding: 20px;
+}
+
+/* Style the list inside the menu */
+nav ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+article {
+  float: left;
+  padding: 20px;
+  width: 80%;
+  background-color: #f1f1f1;
+  height: 300px; /* only for demonstration, should be removed */
+}
+
+/* Clear floats after the columns */
+section:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+
+/* Style the footer */
+footer {
+  background-color: #777;
+  padding: 10px;
+  text-align: center;
+  color: white;
+}
+
+/* Responsive layout - makes the two columns/boxes stack on top of each other instead of next to each other, on small screens */
+@media (max-width: 600px) {
+  nav, article {
+    width: 100%;
+    height: auto;
+  }
+}
+</style>
+
